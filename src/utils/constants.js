40f624f7a65904d1997e05bcb2a51417d582d90a
molmol000001-maker <mol.mol.000001@gmail.com>
2025-12-01 16:58:58 +0900

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+// カクテルの強さプリセット
+export const COCKTAIL_STRENGTHS = [
+  {
+    key: "weak",
+    label: "弱め（5%）",
+    abv: 5,
+    note: "軽めのカクテル",
+  },
+  {
+    key: "normal",
+    label: "普通（10%）",
+    abv: 10,
+    note: "一般的なカクテル",
+  },
+  {
+    key: "strong",
+    label: "強い（20%）",
+    abv: 20,
+    note: "度数高めのカクテル",
+  },
+];
